366172875f3c9a530c47d300bda902f9142ebe56
giulio-pancini <119603778+giulio-pancini@users.noreply.github.com>
2023-06-07 17:54:53 +0200

    Add files via upload

---
+++ cashCard/src/main/java/entity/CashCard.java
@@ -0,0 +1,4 @@
+package entity;
+
+public record CashCard(Long id, Double amount) {
+}
---
+++ cashCard/src/test/java/example/cashCard/CashCardJsonTest.java
@@ -0,0 +1,45 @@
+package example.cashCard;
+
+import entity.CashCard;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CashCardJsonTest {
+
+    @Autowired
+    private JacksonTester<CashCard> json;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void cashCardSerializationTest() throws IOException {
+        CashCard cashCard = new CashCard(99L, 123.45);
+        assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
+        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
+        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id").isEqualTo(99);
+        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
+        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
+    }
+
+    @Test
+    public void cashCardDeserializationTest() throws IOException {
+        String expected = """
+                {
+                    "id":99,
+                    "amount":123.45
+                }
+                """;
+        assertThat(json.parseObject(expected)).isEqualTo(new CashCard(99L, 123.45));
+        assertThat(json.parseObject(expected).id()).isEqualTo(99);
+        assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
+    }
+}
